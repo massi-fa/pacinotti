0c3c20cc25bda06a2653c49dc62d44986a685b30
Massimiliano Fadda <72360779+massi-fa@users.noreply.github.com>
2023-11-07 20:04:27 +0100

    Counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 //Dichiarazione Styled Components:
+const Div = styled.div`
+  margin: 100px auto;
+  padding: 50px;
+  background-color: #66A182;
+  max-width: 500px;
+  border: 2px solid #2E4057;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+`;
+
 const H1 = styled.h1`
-  color: red;
+  color: #2E4057;
+  width: 200px;
+  margin: auto;
   text-align: center;
   font-size: 70px;
-`
+`;
+
+const Button = styled.button`
+  margin: 20px auto;
+  background-color: #CAFFB9;
+  color: #2E4057;
+  font-size: 20px;
+  border: 2px solid #2D5741;
+  border-radius: 5px;
+  padding: 10px;
+  width: 200px;
+`;
 
 const App = () => {
+  //Dichiarazione Variabili stato:
+  const [state, setState] = useState(0)
 
   //Chiamate e Dichiarazioni Funzioni:
-  console.log('Hello World')
+  const contatore = () => {
+    setState(state + 1);
+    console.log(state);
+  };
+
+  const reset = () => {
+    setState(0);
+    console.log(state);
+  };
 
   //Struttura del componente (HTML):
   return(
-    <div>
-      <H1>Hello World</H1>
-    </div>
+    <Div>
+      <H1>{state}</H1>
+      <Button onClick={contatore}>
+        Click Me!
+      </Button>
+      <Button onClick={reset}>
+        Reset Me!
+      </Button>
+    </Div>
   )
-}
+};
 
 export default App;
